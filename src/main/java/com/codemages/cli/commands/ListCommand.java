2d5e344ba49b0d85f279737c9e4e37baf312e1ca
Wesley Prado <wesleyprado.dev@gmail.com>
2024-10-28 21:45:59 -0300

    feat(ListCommand): list all tasks or by status

--- src/main/java/com/codemages/cli/commands/ListCommand.java
+++ src/main/java/com/codemages/cli/commands/ListCommand.java
@@ -1,6 +1,7 @@
 package com.codemages.cli.commands;
 
 import com.codemages.cli.commands.interfaces.Command;
+import com.codemages.cli.tasks.entities.enums.TaskStatus;
 import com.codemages.cli.tasks.services.TaskService;
 
 import java.util.List;
@@ -13,6 +14,26 @@ public class ListCommand implements Command {
 	}
 
 	public void execute(List<String> args) {
-		taskService.list();
+		if (args.isEmpty()) {
+			taskService.list();
+			return;
+		}
+
+		String status = args.getFirst();
+
+		switch (status.toLowerCase()) {
+			case "todo":
+				taskService.listByStatus(TaskStatus.TODO);
+				break;
+			case "in-progress":
+				taskService.listByStatus(TaskStatus.IN_PROGRESS);
+				break;
+			case "done":
+				taskService.listByStatus(TaskStatus.DONE);
+				break;
+			default:
+				System.err.println(
+						"Invalid status. Use todo, in-progress or done");
+		}
 	}
 }
