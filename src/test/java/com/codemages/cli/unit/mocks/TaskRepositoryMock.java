f36474857ed23747b124927804558ef3ff1a5e3f
Wesley Prado <wesleyprado.dev@gmail.com>
2024-10-30 23:18:14 -0300

    refactor: remove deleted DqlException reference

--- src/test/java/com/codemages/cli/unit/mocks/TaskRepositoryMock.java
+++ src/test/java/com/codemages/cli/unit/mocks/TaskRepositoryMock.java
@@ -1,7 +1,6 @@
 package com.codemages.cli.unit.mocks;
 
 import com.codemages.cli.repository.exceptions.DmlException;
-import com.codemages.cli.repository.exceptions.DqlException;
 import com.codemages.cli.repository.interfaces.Repository;
 import com.codemages.cli.tasks.entities.Task;
 import com.codemages.cli.unit.mocks.interfaces.Mocker;
@@ -9,44 +8,45 @@ import com.codemages.cli.unit.mocks.interfaces.Mocker;
 import java.util.List;
 
 public class TaskRepositoryMock extends Mocker implements Repository<Task> {
-	@Override
-	public void insert(Task task) throws DmlException {
-		saveMethodAndArgs("insert", new Object[]{task});
-	}
+		@Override
+		public void insert(Task task) throws DmlException {
+				saveMethodAndArgs("insert", new Object[]{task});
+		}
 
-	@Override public void update(Task object) throws DmlException {
-		saveMethodAndArgs("update", new Object[]{object});
-	}
+		@Override
+		public void update(Task object) throws DmlException {
+				saveMethodAndArgs("update", new Object[]{object});
+		}
 
-	@Override
-	public void delete(int id) throws DmlException {
-		saveMethodAndArgs("delete", new Object[]{id});
-	}
+		@Override
+		public void delete(int id) throws DmlException {
+				saveMethodAndArgs("delete", new Object[]{id});
+		}
 
-	@Override
-	public Task getById(int id) throws DqlException {
-		saveMethodAndArgs("getById", new Object[]{id});
-		return new Task(
-				id,
-				"Task description",
-				null,
-				null,
-				null
-		);
-	}
+		@Override
+		public Task getById(int id) {
+				saveMethodAndArgs("getById", new Object[]{id});
+				return new Task(
+								id,
+								"Task description",
+								null,
+								null,
+								null
+				);
+		}
 
-	@Override
-	public List<Task> getAll() throws DqlException {
-		saveMethodAndArgs("getAll", new Object[]{});
+		@Override
+		public List<Task> getAll() {
+				saveMethodAndArgs("getAll", new Object[]{});
 
-		return List.of(
-				new Task(
-						1,
-						"Task description",
-						null,
-						null,
-						null
-				)
-		);
-	}
+				return List.of(
+								new Task(
+												1,
+												"Task description",
+												null,
+												null,
+												null
+								)
+				);
+		}
 }
