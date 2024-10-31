69c5b9fc1cf4780d33668d5c680fcba41f3bfb65
Wesley Prado <wesleyprado.dev@gmail.com>
2024-10-30 23:21:11 -0300

    refactor: remove unused class

--- src/main/java/com/codemages/cli/tasks/infra/repository/abstracts/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codemages.cli.tasks.infra.repository.abstracts;
-
-import com.codemages.cli.repository.interfaces.Repository;
-import com.codemages.cli.tasks.entities.Task;
-
-public abstract class TaskRepository implements Repository<Task> {
-
-}
