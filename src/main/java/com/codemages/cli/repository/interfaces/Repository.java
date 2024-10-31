309a3837aa692fbd90d56b0501164cd9bcec5fcf
Wesley Prado <wesleyprado.dev@gmail.com>
2024-10-30 23:18:04 -0300

    refactor: remove deleted DqlException reference

--- src/main/java/com/codemages/cli/repository/interfaces/Repository.java
+++ src/main/java/com/codemages/cli/repository/interfaces/Repository.java
@@ -1,19 +1,17 @@
 package com.codemages.cli.repository.interfaces;
 
 import com.codemages.cli.repository.exceptions.DmlException;
-import com.codemages.cli.repository.exceptions.DqlException;
-import com.codemages.cli.tasks.entities.Task;
 
 import java.util.List;
 
 public interface Repository<T> {
-	void insert(T object) throws DmlException;
+		void insert(T object) throws DmlException;
 
-	void update(T object) throws DmlException;
+		void update(T object) throws DmlException;
 
-	void delete(int id) throws DmlException;
+		void delete(int id) throws DmlException;
 
-	T getById(int id) throws DqlException;
+		T getById(int id);
 
-	List<T> getAll() throws DqlException;
+		List<T> getAll();
 }
