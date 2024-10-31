ab359186cc3573e5e301801695a87767ed3883d9
Wesley Prado <wesleyprado.dev@gmail.com>
2024-10-30 22:45:31 -0300

    feat: add enum status validation method

--- src/main/java/com/codemages/cli/tasks/entities/enums/TaskStatus.java
+++ src/main/java/com/codemages/cli/tasks/entities/enums/TaskStatus.java
@@ -1,5 +1,12 @@
 package com.codemages.cli.tasks.entities.enums;
 
+import java.util.Arrays;
+
 public enum TaskStatus {
-	TODO, IN_PROGRESS, DONE
+	TODO, IN_PROGRESS, DONE;
+
+	public static boolean isValid(String value) {
+		return Arrays.stream(TaskStatus.values())
+				.anyMatch(status -> status.name().equals(value));
+	}
 }
